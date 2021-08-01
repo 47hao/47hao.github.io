8c174cd8c6f850e0f1663942cd242d61e8e1e008
Andrew Zhao <andrewhzhao@gmail.com>
2021-08-01 16:02:10 -0400

    github button updates

--- collapsible.js
+++ collapsible.js
@@ -1,12 +1,10 @@
 var cols = document.getElementsByClassName('collapsible');
 var i;
-console.log("script loaded");
-console.log("found: " + cols.length);
 
 for (i = 0; i < cols.length; i++) {
     console.log("detected collapsible");
     cols[i].addEventListener("click", function() { //click happened on the entire button
-      this.children[1].classList.toggle("active");
+      this.children[1].classList.toggle('active');
       this.children[0].classList.toggle('down');
       var content = this.nextElementSibling;
       if (content.style.maxHeight) {
